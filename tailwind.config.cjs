96818d1d2ee0d736d02a1d284ea1fb212cd4ba86
Filip Bączkowski <filo.b@wp.pl>
2023-08-21 21:49:56 +0200

    feat: add tailwindcss config variables

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,37 @@
 export default {
   content: ["index.html", "./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: {
+        400: "#F5F0EC",
+        500: "#ffffff",
+      },
+      black: "#111",
+      accent: {
+        green: "#1B5B31",
+        blue: "#003EDC",
+        apricot: {
+          100: "#F5F0EC",
+          500: "#DCC1AB",
+          600: "#b09a89",
+        },
+      },
+    },
     screens: {
       mobile: "320px",
-      "mobile-md": { min: "450px", max: "768px" },
+      "mobile-md-end": { min: "450px", max: "767px" },
+      "mobile-end": { min: "320px", max: "767px" },
       tablet: "768px",
+      "tablet-end": { min: "768px", max: "1279px" },
       laptop: "1280px",
       desktop: "1440px",
     },
-
+    fontFamily: {
+      Montserrat: ["Montserrat", "sans-serif"],
+      body: ["Inter", "sans-serif"],
+    },
     extend: {
       padding: {
         22: "5.56rem",
@@ -18,20 +41,26 @@ export default {
       },
 
       backgroundImage: {
-        "nav-button": "url('/src/assets/svg/nav-icon.svg')",
-        "icon-button-search": "url('/src/assets/svg/loop-icon.svg')",
-        "icon-button-burger": "url('/src/assets/svg/burger-menu-icon.svg')",
+        "icon-nav-arrow": "url('/src/assets/svg/nav-arrow-icon.svg')",
+        "icon-search": "url('/src/assets/svg/loop-icon.svg')",
+        "icon-burger": "url('/src/assets/svg/burger-menu-icon.svg')",
+        "icon-arrow": "url('/src/assets/svg/arrow-icon.svg')",
+        gradient:
+          "linear-gradient(360deg, #DCC1AB 0%, rgb(214 183 158 / 0) 100%)",
       },
       gridTemplateRows: {
         hero: "1fr ",
       },
       fontSize: {
-        "6xl": [
-          "3.75rem",
+        small: [
+          "0.875rem",
           {
-            lineHeight: "4.375rem",
+            letterSpacing: "0.14px",
           },
         ],
+      },
+      letterSpacing: {
+        tightest: "-.01rem",
       },
     },
   },
